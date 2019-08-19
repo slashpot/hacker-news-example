8fcd7df191045a388f4d7f69822bdea09125e43d
Eric Lin <ttttt123465@gmail.com>
2019-08-19 21:22:51 +0800

    add empty tests

---
+++ hacker-news-example/tests/unit/App.test.js
@@ -0,0 +1,21 @@
+describe('given App component', () => {
+    it('should show "search..." in text placeholder at beginning', () => {
+        
+    });
+
+    it('should show "please type keyword" in text placeholder when searching with empty keyword', () => {
+        
+    });
+
+    it('should call getNews api when searching with valid keyword', () => {
+        
+    });
+
+    it('should show apiError msg in text placeholder when get error of getNews api', () => {
+        
+    });
+
+    it('should clear keyword after searching', () => {
+        
+    });
+});
---
+++ hacker-news-example/tests/unit/getNews.test.js
@@ -0,0 +1,10 @@
+describe('given searching news with keyword react', () => {
+    
+    it('should only get titles when response normally', () => {
+        
+    });
+
+    it('should throw exception with apiError message when request failed', () => {
+        
+    });
+})
