69ac490b1c17a6cf304c712d8cb53b19d52086ab
Eric Lin <ttttt123465@gmail.com>
2019-08-20 10:12:11 +0800

    fix bug

--- hacker-news-example/src/helper/apiUtility.js
+++ hacker-news-example/src/helper/apiUtility.js
@@ -5,6 +5,6 @@ export default {
         return await fetch(`http://hn.algolia.com/api/v1/search?query=${keyword}`)
             .then((res) => res.json())
             .then((data) => data.hits.map((x) => x.title))
-            .catch(() => { throw new exception(msgType.apiError); });
+            .catch(() => { throw new Error(msgType.apiError); });
     }
 }
