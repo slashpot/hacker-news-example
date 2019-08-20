c0400f32109ce31036fa03c67f51b2cc81451e41
Eric Lin <ttttt123465@gmail.com>
2019-08-20 20:53:38 +0800

    finish 1 test

--- hacker-news-example/tests/unit/App.spec.js
+++ hacker-news-example/tests/unit/App.spec.js
@@ -1,5 +1,5 @@
 import { createLocalVue, mount } from '@vue/test-utils';
-import App from '../../src/App.vue'
+import App from '../../src/App.vue';
 import { msgType } from '../../src/helper/enum';
 
 describe('given App component', () => {
@@ -15,14 +15,33 @@ describe('given App component', () => {
     });
 
     it('should call getNews api when searching with valid keyword', () => {
-        
+        const getNews = jest.fn();
+        jest.doMock('../../src/helper/apiUtility', () => ({ getNews }));
+        const AppNew = require('../../src/App.vue').default;
+        const wrapper = mount(AppNew);
+
+        wrapper.vm.$data.keyword = 'react';
+        wrapper.vm.search();
+
+        expect(getNews.mock.calls.length).toBe(1);
+        expect(getNews.mock.calls[0]).toEqual(['react']);
     });
 
     it('should show apiError msg in text placeholder when get error of getNews api', () => {
-        
+        const getNews = jest.fn(()=>{throw new Error(msgType.apiError)});
+        jest.doMock('../../src/helper/apiUtility', () => ({ getNews }));
+        const AppNew = require('../../src/App.vue').default;
+        const wrapper = mount(AppNew);
+
+        wrapper.vm.$data.keyword = 'react';
+        wrapper.vm.search();
+
+        expect(wrapper.vm.$data.hint).toBe(msgType.apiError);
     });
 
     it('should clear keyword after searching', () => {
-        
+
     });
+
+    afterEach(jest.resetModules);
 });
