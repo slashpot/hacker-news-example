3f0e66e9c239f93a40e52bddbc43e0dbf25e63bb
Eric Lin <ttttt123465@gmail.com>
2019-09-16 20:34:44 +0800

    use https instead of http

--- hacker-news-example/src/helper/apiUtility.js
+++ hacker-news-example/src/helper/apiUtility.js
@@ -2,7 +2,7 @@ import { msgType } from "./enum";
 
 export default {
     async getNews(keyword) {
-        return await fetch(`http://hn.algolia.com/api/v1/search?query=${keyword}`)
+        return await fetch(`https://hn.algolia.com/api/v1/search?query=${keyword}`)
             .then((res) => res.json())
             .then((data) => data.hits.map((x) => {return {title: x.title, url: x.url}}))
             .catch(() => { throw new Error(msgType.apiError); });
